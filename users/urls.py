4363062bd6deba9b75453f18104078216d5f1b83
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-03-23 04:46:05 +1100

    lesson_5_fix

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 urlpatterns = [
     path('registration/',views.registration_api_view),
     path('confirmation/',views.confirmation_api_view),
-    path('authorizationgit pull origin master/', views.authorization_api_view),
+    path('authorizationgit/', views.authorization_api_view),
 
 ]
 
