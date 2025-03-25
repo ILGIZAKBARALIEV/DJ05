646c36e13d6c458f4819918a6ed6f82902715d1f
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-03-26 03:12:44 +1100

    def_change_to_class

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -5,7 +5,7 @@ from utils.constants import LIST_CREATE,RETRIEVE_UPDATE_DESTROY
 
 urlpatterns = [
     path('DirectorModel/',views.DirectorListAPIView.as_view()),
-    path('DirectorModel/<int:id>/',views.DirectorDetailAPIView.as_view()),
+    path('DirectorModel1/<int:id>/',views.DirectorDetailAPIView.as_view()),
     path('MoviesModel/',views.MoviesListAPIView.as_view()),
     path('MoviesModel/<int:id>/',views.MoviesDetailAPIView.as_view()),
     path('ReviewModel/',views.ReviewsListAPIView.as_view()),
