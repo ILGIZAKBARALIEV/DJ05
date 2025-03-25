1d1e02d347c82813309c8a762f57694055911520
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-03-26 03:03:41 +1100

    def_change_to_class_details_doesn't_work

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -5,9 +5,9 @@ from utils.constants import LIST_CREATE,RETRIEVE_UPDATE_DESTROY
 
 urlpatterns = [
     path('DirectorModel/',views.DirectorListAPIView.as_view()),
-    path('<int:id>/',views.DirectorDetailAPIView.as_view()),
+    path('DirectorModel/<int:id>/',views.DirectorDetailAPIView.as_view()),
     path('MoviesModel/',views.MoviesListAPIView.as_view()),
-    path('<int:id>/',views.MoviesDetailAPIView.as_view()),
+    path('MoviesModel/<int:id>/',views.MoviesDetailAPIView.as_view()),
     path('ReviewModel/',views.ReviewsListAPIView.as_view()),
-    path('<int:id>/',views.ReviewsDetailAPIView.as_view()),
+    path('ReviewModel/<int:id>/',views.ReviewsDetailAPIView.as_view()),
 ]
