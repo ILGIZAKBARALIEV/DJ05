4f3a0751eb38c9d35929fe1c5338b74d366ea3fb
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-03-28 13:20:41 +1100

    secret_key_swagger_env

--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import swagger
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/movie_app/', include('movie_app.urls')),
     path('api/v1/users/', include('users.urls')),
 
 ]
+urlpatterns += swagger.urlpatterns
